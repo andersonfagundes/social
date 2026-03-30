99969bd23c6527baf3076e17e3b03bb6422cac64
Anderson <anderson.fagundes4@gmail.com>
2026-03-30 12:08:10 +0100

    ✨ feat: implement basic HTTP server with health check

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 type application struct {
 	config config
@@ -10,13 +14,24 @@ type config struct {
 	addr string
 }
 
-func (app *application) run() error{
+func (app *application) mount() *http.ServeMux {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error{
 	
 	srv := &http.Server{
 		Addr: app.config.addr,
 		Handler: mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout: time.Second * 10,
+		IdleTimeout: time.Minute,
 	}
+
+	log.Printf("server has started at %s", app.config.addr)
 
 	return srv.ListenAndServe()
 }
